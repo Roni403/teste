11e8aac39fc83944abe0a8a9f8e00961202d41f3
Ronilson Mendes Ferreira <04476176135@ifb.local>
2025-09-03 10:29:36 -0300

    novo comite

--- pasta1/arquivo1.py
+++ pasta1/arquivo1.py
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello good bay ")
 
+=======
+print("Hello World")
+>>>>>>> parent of a2e814c (mudei pata good)
 =======
 print("Hello World")
 >>>>>>> parent of a2e814c (mudei pata good)
