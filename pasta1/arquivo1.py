0409001aa954e6da95b6a302f5d1e43a3838b9d3
Ronilson Mendes Ferreira <04476176135@ifb.local>
2025-09-03 10:45:51 -0300

    novo

--- pasta1/arquivo1.py
+++ pasta1/arquivo1.py
@@ -1,4 +1,7 @@
 print("Hello good bay ")
+<<<<<<< HEAD
 print("Hello World")
 parent of a2e814c (mudei pata good)
 print("Hello World") parent of a2e814c (mudei pata good)
+=======
+>>>>>>> parent of 9a0f4c4 (mudei)
